314a46a5a93d5e1349571badece391b580195155
czelijm <czelijm@outlook.com>
2020-11-14 13:24:52 +0100

    Add project files.

---
+++ BookListRazor/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BookListRazor.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
